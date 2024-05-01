f59729a6db67db9446ce2bb34b264e912601fe2e
Fernando <f.henrique.fraga@gmail.com>
2024-05-01 09:15:38 -0300

    pasta principal do projeto, com modelagem do banco, arquivos em python da aplicação, etc

---
+++ store/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+
+	path('update_item/', views.updateItem, name="update_item"),
+	path('process_order/', views.processOrder, name="process_order"),
+    path('about/', views.about, name='about'), 
+    path('courses/python', views.python, name='python'), 
+    path('courses/rust', views.rust, name='rust'), 
+    path('courses/ruby', views.ruby, name='ruby'), 
+    path('courses/data_science', views.data_science, name='data_science'), 
+    path('courses/c', views.c, name='c'), 
+    path('courses/agile', views.agile, name='agile'), 
+    path('finish/', views.finish, name='finalizar'), 
+
+]
---
+++ store/views.py
@@ -0,0 +1,120 @@
+import datetime
+import json
+
+from django.http import JsonResponse
+from django.shortcuts import render
+
+from .models import *
+from .utils import cartData, guestOrder
+
+
+def store(request):
+	data = cartData(request)
+
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+
+	products = Product.objects.all()
+	context = {'products':products, 'cartItems':cartItems}
+	return render(request, 'store/store.html', context)
+
+
+def cart(request):
+	data = cartData(request)
+
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
+	return render(request, 'store/cart.html', context)
+
+def checkout(request):
+	data = cartData(request)
+	
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
+	return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+	data = json.loads(request.body)
+	productId = data['productId']
+	action = data['action']
+	print('Action:', action)
+	print('Product:', productId)
+
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
+
+	orderItem.save()
+
+	if orderItem.quantity <= 0:
+		orderItem.delete()
+
+	return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+	else:
+		customer, order = guestOrder(request, data)
+
+	total = float(data['form']['total'])
+	order.transaction_id = transaction_id
+
+	if total == order.get_cart_total:
+		order.complete = True
+	order.save()
+
+	if order.shipping == True:
+		ShippingAddress.objects.create(
+		customer=customer,
+		order=order,
+		address=data['shipping']['address'],
+		city=data['shipping']['city'],
+		state=data['shipping']['state'],
+		zipcode=data['shipping']['zipcode'],
+		)
+
+	return JsonResponse('Payment submitted..', safe=False)
+
+
+def about(request):
+    return render(request, 'store/about.html') 
+
+def python(request):
+    return render(request, 'store/courses/python.html')  
+
+def rust(request):
+    return render(request, 'store/courses/rust.html')  
+
+def ruby(request):
+    return render(request, 'store/courses/ruby.html')  
+
+def agile(request):
+    return render(request, 'store/courses/agile.html')  
+
+def c(request):
+    return render(request, 'store/courses/c.html')  
+
+def data_science(request):
+    return render(request, 'store/courses/data_science.html')  
+
+def finish(request):
+    return render(request, 'store/finish.html')  
