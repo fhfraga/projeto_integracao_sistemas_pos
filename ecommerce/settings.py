a52151a1c17a4d8d193dcde1bb30ebb5eaeb76d9
Fernando <f.henrique.fraga@gmail.com>
2024-05-02 17:36:23 -0300

    adição da API no projeto

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,8 +23,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'store.apps.StoreConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,6 +6,8 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    path('api/', include('api.urls')),
+    path('api/auth', include('rest_framework.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
